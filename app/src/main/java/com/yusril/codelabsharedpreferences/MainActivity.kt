231a088ec25f77a64b27a5979320491c105af109
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-04-01 21:22:00 +0700

    Initial commit

---
+++ app/src/main/java/com/yusril/codelabsharedpreferences/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yusril.codelabsharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
